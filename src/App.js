37fdfb345743b534de2736d2cd5932c30f4c7095
konarsada <sadabratak@gmail.com>
2022-06-06 01:28:08 +0530

    connect() in redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {connect} from "react-redux"
+import {increment, decrement} from "./redux"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {    
+    return (
+        <div>
+            <h1>{props.count}</h1>
+            <button onClick={props.decrement}>-</button>
+            <button onClick={props.increment}>+</button>
+        </div>
+    )
 }
 
-export default App;
+function mapStateToProps(state) {
+    return {
+        count: state
+    }
+}
+
+const mapDispatchToProps = {
+    increment: increment,
+    decrement: decrement
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+/*
+// more on connect
+// https://react-redux.js.org/api/connect#connect
+
+function mapStateToProps(globalState) {
+    // Takes the global state from Redux as a parameter
+    // returns an object where the keys are the name of the prop your component wants,
+    // values are the actual parts of the global state your component wants
+    
+    return {
+        bananas: "Hello", // accessible as props.bananas
+        apple: globalState // accessible as props.apple
+    }
+}
+
+const mapDispatchToProps = {
+    // key = name of the prop our component will receives
+    // value = action that we want to dispatch to the reducer
+    
+    increment: increment, // accessible as props.increment
+    decrement: decrement // accessible as props.decrement
+}
+
+export default connect(
+    What parts of state do you want? OR mapStateToProps,
+    What actions to dispatch? or mapDispatchToProps
+)(App)
+*/
