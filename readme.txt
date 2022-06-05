f9ab8f692017f3898cb1fd38240d644d18c03354
konarsada <sadabratak@gmail.com>
2022-06-06 01:39:07 +0530

    useSelector(), useDispatch() hooks

---
+++ readme.txt
@@ -0,0 +1,6 @@
+commit 1: connect() in redux
+commit 2: useSelector(), useDispatch() hooks
+
+useSelector(), useDispatch() hooks vs connect in redux
+// https://thoughtbot.com/blog/using-redux-with-react-hooks
+// https://react-redux.js.org/api/hooks#usage-warnings
--- src/App.js
+++ src/App.js
@@ -1,56 +1,22 @@
 import React from "react"
-import {connect} from "react-redux"
+import {connect, useSelector, useDispatch} from "react-redux"
 import {increment, decrement} from "./redux"
 
-function App(props) {    
+function App(props) {
+    // get pieces of state from the globalState
+    const count = useSelector(state => state)
+
+    const dispatch = useDispatch()
+
     return (
         <div>
-            <h1>{props.count}</h1>
-            <button onClick={props.decrement}>-</button>
-            <button onClick={props.increment}>+</button>
+            <h1>{count}</h1>
+            <button onClick={() => dispatch(decrement())}>-</button>
+            <button onClick={() => dispatch(increment())}>+</button>
         </div>
     )
 }
 
-function mapStateToProps(state) {
-    return {
-        count: state
-    }
-}
+export default App
 
-const mapDispatchToProps = {
-    increment: increment,
-    decrement: decrement
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
-
-/*
-// more on connect
-// https://react-redux.js.org/api/connect#connect
-
-function mapStateToProps(globalState) {
-    // Takes the global state from Redux as a parameter
-    // returns an object where the keys are the name of the prop your component wants,
-    // values are the actual parts of the global state your component wants
-    
-    return {
-        bananas: "Hello", // accessible as props.bananas
-        apple: globalState // accessible as props.apple
-    }
-}
-
-const mapDispatchToProps = {
-    // key = name of the prop our component will receives
-    // value = action that we want to dispatch to the reducer
-    
-    increment: increment, // accessible as props.increment
-    decrement: decrement // accessible as props.decrement
-}
-
-export default connect(
-    What parts of state do you want? OR mapStateToProps,
-    What actions to dispatch? or mapDispatchToProps
-)(App)
-*/
+// export default connect(state => ({count: state}), {increment, decrement})(App)
